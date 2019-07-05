59c84e5d37e8dc1dafc6d2ac787115c1c1960deb
radgra <rgrabos@inwerken.de>
2019-07-05 13:32:33 +0200

    working tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,17 +2,26 @@ import unittest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
-from models.student import Student
-
+from models.teacher import Teacher
+from models.user import UserModel
+from seeding_data import teachers
+from app import app
+from db import db
+from flask import Flask
 
 Base = declarative_base()
 
 class TestTeacher(unittest.TestCase):
     def setUp(self):
-        self.engine = create_engine('sqlite:///:memory:')
-        self.session = Session(engine)
-        Base.metadata.create_all(engine)
-
+        self.app = Flask(__name__)
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+        db.init_app(self.app)
+        
+        app.app_context().push() 
+        db.create_all()
+        self.populate_db()
+    
+    def populate_db(self):
         for teacher in teachers:
             user = UserModel.query.filter_by(username=teacher.pop('username')).first()
 
@@ -23,5 +32,14 @@ class TestTeacher(unittest.TestCase):
             
             if user:
                 new_teacher = Teacher(**teacher, user_id=user.id)
-                self.session.add(new_teacher)
-                self.session.commit()
+                db.session.add(new_teacher)
+                db.session.commit()
+
+    def testSomething(self):
+
+        print(Teacher.query.all())
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
